eef79870e4b5b6babb93939bf64fa7d8de6400c7
Celestine Wangechi <wangechicelestine@gmail.com>
2023-11-01 18:37:24 -0700

    Binary Trees

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,8 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
-#include <stdlib.h>
+#include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -10,15 +11,15 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
-
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-#endif
+
+#endif /* _BINARY_TREES_H_ */
